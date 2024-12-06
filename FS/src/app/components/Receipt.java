c4459e068fab8c7773afe17b15a5bd8a5a1116a8
Polyspyder <160246422+Polyspyder@users.noreply.github.com>
2024-12-07 00:44:13 +0800

    Adding receipt component content, still unfinished

---
+++ FS/src/app/components/Receipt.java
@@ -0,0 +1,22 @@
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.entities.Receipt;
+import app.repositories.ReceiptRepository;
+
+public class Receipt{
+    @Autowired
+    ReceiptRepository repo;
+
+    @PostConstruct
+	public void doneInstantiating()
+	{System.out.println(repo);}
+
+    //Functions
+    public Receipt createReceipt()
+    {
+        Receipt a = new Receipt();
+        //will add stuff later
+    }
+}
